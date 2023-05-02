fecc0a9138c3febc949e997f05ab4e4d676a7578
Daniel Lopes <danielorkae@gmail.com>
2023-05-02 13:38:02 -0300

    add layer for presentations

---
+++ include/presentation/bus-presentation.hpp
@@ -0,0 +1,12 @@
+#ifndef PRESENTATION_BUS_PRESENTATION_HPP
+#define PRESENTATION_BUS_PRESENTATION_HPP
+
+#include <vector>
+#include "../../src/domain/bus.cpp"
+
+using namespace std;
+
+void print_bus(Bus bus);
+void print_buses(vector<Bus> buses);
+
+#endif
---
+++ src/presentation/bus-presentation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "bus-presentation.hpp"
+
+void print_bus(Bus bus)
+{
+    cout << "Nome: " << bus.name << endl
+         << "Terminal: " << bus.terminal << endl
+         << endl;
+}
+
+void print_buses(vector<Bus> buses)
+{
+    for (int i = 0; i < buses.size(); i++)
+    {
+        cout << "Ônibus " << i + 1 << endl;
+        print_bus(buses[i]);
+    }
+}
