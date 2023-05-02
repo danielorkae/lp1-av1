dd29da6c0fbf71dee986cbcd536c0eb2d2e272d3
Daniel Lopes <danielorkae@gmail.com>
2023-05-02 01:32:43 -0300

    add feedback dao

---
+++ src/dao/feedback-dao.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <fstream>
+#include "../domain/feedback.cpp"
+#include "../utils/files-utils.cpp"
+
+using namespace std;
+
+/// @brief Retorna todas as reclamações cadastradas.
+/// @return
+vector<Feedback> get_feedbacks()
+{
+    vector<Feedback> feedbacks;
+
+    vector<string> lines = read_file_lines("data/feedbacks.txt");
+
+    // Transforma as linhas em dados das reclamações.
+    for (int i = 0; i < lines.size(); i++)
+    {
+        // A ordem, nesse caso, é importante, pois é a mesma ordem que será salva.
+        Feedback feedback;
+        feedback.content = lines[i];
+        feedback.date = lines[++i];
+
+        feedbacks.push_back(feedback);
+    }
+
+    return feedbacks;
+}
+
+/// @brief Salva todas as reclamações cadastradas.
+/// @param feedbacks
+void save_feedbacks(vector<Feedback> feedbacks)
+{
+    vector<string> lines;
+
+    // Transforma os dados das reclamações em linhas.
+    for (int i = 0; i < feedbacks.size(); i++)
+    {
+        // A ordem, nesse caso, é importante, pois é a mesma ordem que será lida.
+        lines.push_back(feedbacks[i].content);
+        lines.push_back(feedbacks[i].date);
+    }
+
+    save_file_lines("data/feedbacks.txt", lines);
+}
